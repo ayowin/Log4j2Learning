cf5d2e0a5444de9f980cde2248d8e2c7e58f49da
OuyangWei <18344175265@126.com>
2019-02-15 18:40:53 +0800

    2019-2-15 18:40 Log4j2学习demo

---
+++ src/com/wz/log4j2/test/Test.java
@@ -0,0 +1,43 @@
+package com.wz.log4j2.test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
+public class Test {
+
+    public static Logger logger = (Logger) LogManager.getLogger(Test.class);
+
+    public static void main(String[] args) {
+
+	for(int i = 0; i < 1000; i++) {
+	    logger.trace("trace level");
+	    logger.debug("debug level");
+	    logger.info("info level");
+	    logger.warn("warn level");
+	    logger.error("error level");
+	    logger.fatal("fatal level");
+	}
+
+	Thread t1 = new Thread(new Runnable() {
+
+	    public void run() {
+		for(int i = 0; i<5 ; i++) {
+		    logger.error("t1 error");
+		}
+	    }
+	});
+	t1.start();
+
+	Thread t2 = new Thread(new Runnable() {
+
+	    public void run() {
+		for(int i = 0; i<5 ; i++) {
+		    logger.error("t2 error");
+		}
+	    }
+	});
+	t2.start();
+
+    }
+}
+
